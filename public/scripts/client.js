3f8b5c258405e70f5f93e0ed20f93ce82932a777
juliasut <juliasutygina@gmail.com>
2021-07-14 20:13:07 -0600

    Implement createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,28 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = (tweet) => {
+
+  const $tweet = $(`<article class="tweet">
+  <header>
+    <div>
+      <img src="https://i.imgur.com/nlhLi3I.png">
+      <span>Katy Perry</span>
+    </div>
+    <span class="user-id">@userId</span>
+  </header>
+
+  <p>This is the latest tweet showing up.</p>
+  <hr>
+  <footer>
+    <span>created 10 days ago</span>
+    <span>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </span>
+  </footer>
+ </article>`);
+
+ return $tweet;
+};
