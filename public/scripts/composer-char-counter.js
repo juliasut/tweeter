a717a63b83aebca39b23776e13db11833f16e803
juliasut <juliasutygina@gmail.com>
2021-07-15 15:33:45 -0600

    Declare onIput function outside .ready

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,16 @@
 
+// assign eventListener -> tweetText is reffered to as 'this' in the function
 $(document).ready(function() {
   
-  const $tweetText = $('#tweet-text');
+  $('#tweet-text').on('input', onInput);
   
-  // assign eventListener -> tweetText is reffered to as 'this' in the function
-  $tweetText.on('input', function() {
-    
-    const tweetCharRemaining = 140 - $(this).val().length; 
-    const $counter = $(this).siblings('div').children('.counter');
-
-    $counter.html(tweetCharRemaining);
-    
-    // if message length is invalid -> counter turns red
-    tweetCharRemaining < 0 ? $counter.addClass('invalid') : $counter.removeClass('invalid')
-  });
 });
+
+// Count chars for new tweet and turn counter red if over the limit
+const onInput = function() {
+  const tweetCharRemaining = 140 - $(this).val().length; 
+  const $counter = $(this).siblings('div').children('.counter');
+  $counter.html(tweetCharRemaining);
+  // if message length is invalid -> counter turns red
+  tweetCharRemaining < 0 ? $counter.addClass('invalid') : $counter.removeClass('invalid')
+};
