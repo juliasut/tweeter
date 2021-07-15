24f3b20db5419f5fc5649233c8abe2475dbd20e6
juliasut <juliasutygina@gmail.com>
2021-07-14 22:05:29 -0600

    Add renderTweets function to show the tweet stream

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -23,8 +23,8 @@ $(document).ready(function () {
   
 
 const createTweetElement = (tweet) => {
-
-  const $tweet = $(`
+  //HTML for new node:
+  let $tweet = $(`
   <article class="tweet">
   <header>
     <div>
@@ -50,21 +50,46 @@ const createTweetElement = (tweet) => {
 };
 
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    $tweet = createTweetElement(tweet);
+    $('#tweets-container').prepend($tweet); 
+  }
 }
 
-const $tweet = createTweetElement(tweetData);
+renderTweets(data);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').prepend($tweet); 
+$('form').on('submit', function(event) {
+  // stops HTML from running post req and reloading the whole page
+  event.preventDefault();
+
+  // formats data for the server as a query string 
+  const urlEncodedData = $(this).serialize();
+
+})
 });
