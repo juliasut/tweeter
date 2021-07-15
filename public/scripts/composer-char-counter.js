05038c2d1936750eff9ff597a1869ea6e78104c8
juliasut <juliasutygina@gmail.com>
2021-07-14 22:38:50 -0600

    Remove error comments -> will apply in client.js in form.on

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
   // assign eventListener -> tweetText is reffered to as 'this' in the function
   $tweetText.on('input', function() {
     
-    // console.log($(this).siblings('div').children('.counter').val())
     const tweetCharRemaining = 140 - $(this).val().length; 
     const $counter = $(this).siblings('div').children('.counter');
 
@@ -14,10 +13,5 @@ $(document).ready(function() {
     
     // if message length is invalid -> counter turns red
     tweetCharRemaining < 0 ? $counter.addClass('invalid') : $counter.removeClass('invalid')
-    
-    // error message when submit length 0 or > 140
   });
-
-
-  // $input.val('').focus() - to clear input field and put cursor in the field
 });
